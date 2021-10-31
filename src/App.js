c800f0c17204b8fc77763c0925207f7cbe59a779
Ashley Swank <Ashley@Psychics-MacBook-Pro.local>
2021-10-31 15:17:40 -0400

    WIP add picture assets folder, import and export folder for pics and import pics to App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 
 import Main from "./components/Main";
 import './App.css';
+import { pic1, pic2, pic3, pic4, pic5, pic6, pic7, pic8, pic9, pic10, pic11, pic12, pic13, pic14, pic15, pic16, pic17, pic18, pic19, pic20, pic21 } from "./components/Pics"
 
 
 function App() {
+
+
+
+
+
+
+
  return (
     <div className="App">
        <Main />
---
+++ src/components/Pics.js
@@ -0,0 +1,26 @@
+import pic1 from "../assets/pic1.jpeg"
+import pic2 from "../assets/pic2.jpeg"
+import pic3 from "../assets/pic3.jpeg"
+import pic4 from "../assets/pic4.jpeg"
+import pic5 from "../assets/pic5.jpeg"
+import pic6 from "../assets/pic6.jpeg"
+import pic7 from "../assets/pic7.jpeg"
+import pic8 from "../assets/pic8.jpeg"
+import pic9 from "../assets/pic9.jpeg"
+import pic10 from "../assets/pic10.jpeg"
+import pic11 from "../assets/pic11.jpeg"
+import pic12 from "../assets/pic12.jpeg"
+import pic13 from "../assets/pic13.jpeg"
+import pic14 from "../assets/pic14.jpeg"
+import pic15 from "../assets/pic15.jpeg"
+import pic16 from "../assets/pic16.jpeg"
+import pic17 from "../assets/pic17.jpeg"
+import pic18 from "../assets/pic18.jpeg"
+import pic19 from "../assets/pic19.jpeg"
+import pic20 from "../assets/pic20.jpeg"
+import pic21 from "../assets/pic21.jpeg"
+
+
+
+
+export default { pic1, pic2, pic3, pic4, pic5, pic6, pic7, pic8, pic9, pic10, pic11, pic12, pic13, pic14, pic15, pic16, pic17, pic18, pic19, pic20, pic21 };
